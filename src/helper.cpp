1b0cdc7811ef2c8b7e28c584e0672c6cc25f6838
Magnus Persson <magnus@noreply.github.com>
2022-06-15 21:36:06 +0200

    Linux build fix

--- src/helper.cpp
+++ src/helper.cpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 #include <Ticker.h>
-#include <arduino.h>
+#include <Arduino.h>
 
 #include <helper.hpp>
 
